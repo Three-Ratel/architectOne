2d7023be9a59b9b0b6ba55ee69c088def0dcf0f0
wuxp@imsa.org.cn <wuxp@imsa.org.cn>
2018-04-20 19:54:57 +0800

    day01-0.1

---
+++ day01/Flash框架/2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/index')
+def index():
+    return "H w"
+
+
+
+if __name__ == '__main__':
+    app.run()
